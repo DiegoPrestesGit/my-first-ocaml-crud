dbb8b241ae738225bb41ef473ffce11d98daf5da
Diego Prestes <diegoprestes.git@hotmail.com>
2021-04-21 18:39:17 -0300

    feat: start read_all_messages route

--- crud.ml
+++ crud.ml
@@ -1,3 +1,8 @@
 open Opium
 
-let () = App.empty |> App.run_multicore
+type message = { user_name: string; body: string; }
+let messages = ref []
+(* GET /messages *)
+let read_all_messages = App.get "/messages" (fun _request -> Lwt.return (Response.of_plain_text "x"))
+
+let () = App.empty |> read_all_messages |> App.run_multicore

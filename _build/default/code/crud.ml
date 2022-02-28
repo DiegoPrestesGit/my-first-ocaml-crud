170891d0fa068c3f52118b4bbb4158492afcc76a
Diego Prestes <diegoprestes.git@hotmail.com>
2022-02-28 15:29:59 -0300

    chore: update dependencies, ocaml and esy config

--- _build/default/code/crud.ml
+++
@@ -1,8 +0,0 @@
-open Opium
-
-type message = { user_name: string; body: string; }
-let messages = ref []
-(* GET /messages *)
-let read_all_messages = App.get "/messages" (fun _request -> Lwt.return (Response.of_plain_text "x"))
-
-let () = App.empty |> read_all_messages |> App.run_multicore
